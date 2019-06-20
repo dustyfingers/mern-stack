2b607f66933ff4b34e398969908c7b970e36fd96
dustyfingers <dustyfingers01257@gmail.com>
2019-06-20 16:00:50 -0600

    created profile model

--- DevConnector/routes/api/auth.js
+++ DevConnector/routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
@@ -17,6 +21,63 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server error.')
     }
 });
+
+
+// @route: POST api/auth
+// @desc: authenticate user and get token
+// @access: public
+router.post('/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').exists() // password is required
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            // see if user already exists
+            let user = await User.findOne({ email });
+
+            // if no user found w provided email, invalid credentials error
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            // match found users hashed password to password provided with email in the request 
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            // if passwords dont match, invalid credentials error
+            if (!isMatch) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            }
+
+            // return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
 
 
 module.exports = router;
--- DevConnector/routes/api/users.js
+++ DevConnector/routes/api/users.js
@@ -4,7 +4,7 @@ const { check, validationResult } = require('express-validator/check');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config')
+const config = require('config');
 const User = require('../../models/User');
 
 
