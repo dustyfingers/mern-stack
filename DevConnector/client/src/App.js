4a136197ec8b35f64084c9b0a1dd91274affd9cc
dustyfingers <dustyfingers01257@gmail.com>
2019-07-03 22:30:47 -0600

    redux init setup

--- DevConnector/client/src/App.js
+++ DevConnector/client/src/App.js
@@ -6,17 +6,27 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
-const App = () =>
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Route exact path='/' component={ Landing } />
-            <section className='container'>
-                    <Switch>
-                        <Route exact path='/register' component={ Register } />
-                        <Route exact path='/login' component={ Login } />
-                    </Switch>
-            </section>
-        </Fragment>
-    </Router>
+// redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+const App = () => (
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Route exact path='/' component={ Landing } />
+                <section className='container'>
+                        <Switch>
+                            <Route exact path='/register' component={ Register } />
+                            <Route exact path='/login' component={ Login } />
+                        </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
+);
+
+
 export default App;
